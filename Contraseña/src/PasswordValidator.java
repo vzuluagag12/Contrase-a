590fb81d9bbb56118ebdce420750ece7ef1afce0
vzuluagag12 <144352922+vzuluagag12@users.noreply.github.com>
2024-04-30 22:53:35 -0500

    PasswordValidator.java

--- Contraseña/src/PasswordValidator.java
+++ Contraseña/src/PasswordValidator.java
@@ -1,22 +1,18 @@
 class PasswordValidator {
-    // Método para validar la contraseña
     public static void validatePassword(String password, String confirmPassword) throws PasswordException {
-        // Verificar si las contraseñas coinciden
+
         if (!password.equals(confirmPassword)) {
             throw new PasswordException("Las contraseñas no coinciden.");
         }
         
-        // Verificar la longitud mínima
         if (password.length() < 8) {
             throw new PasswordException("La contraseña debe tener al menos 8 caracteres.");
         }
         
-        // Verificar espacios en blanco
         if (password.contains(" ")) {
             throw new PasswordException("La contraseña no debe contener espacios en blanco.");
         }
         
-        // Verificar si contiene al menos un carácter en mayúscula, un número y un carácter especial
         boolean hasUpperCase = false;
         boolean hasDigit = false;
         boolean hasSpecialChar = false;
